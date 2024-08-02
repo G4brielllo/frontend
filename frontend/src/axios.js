5065d4c7f473b60dc4f11f23a97df6066267fe58
G4brielllo <gabrieltarasiuk3@gmail.com>
2024-08-02 12:22:23 +0200

    Added jwt_token operation

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -8,4 +8,16 @@ const instance = axios.create({
   },
 });
 
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('jwt_token');
+  if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+
+
 export default instance;
